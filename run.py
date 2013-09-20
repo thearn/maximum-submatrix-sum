c85e7ee6a4096a5da1225092d356c20e2058a4c1
Tristan A. Hearn <tristan.a.hearn@nasa.gov>
2013-09-19 20:23:54 -0400

    first commit

---
+++ run.py
@@ -0,0 +1,13 @@
+from algorithms import brute_submatrix_max, fft_submatrix_max
+import numpy as np
+
+#Set matrix dimensions (rows, columns)
+M, N = 23, 29
+
+#Generate MxN matrix of random integers
+A = np.random.randint(-100,100, size=(M, N))
+
+#Test each algorithm
+#output format: maximizing subarray slice specification, maximum sum value, running time
+print fft_submatrix_max(A)
+print brute_submatrix_max(A)
